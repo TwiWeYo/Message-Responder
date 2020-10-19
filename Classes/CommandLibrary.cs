e932aaedf87c44c06c5ca22578167181d7c399c5
TwiWeYo <portportpor@gmail.com>
2020-10-19 23:29:39 +0300

    Добавьте файлы проекта.

---
+++ Classes/CommandLibrary.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace PlsRespond.Classes
+{
+    public class CommandLibrary
+    {
+        private static string path;
+        private static List<Regex> CommandList = new List<Regex>();
+        static CommandLibrary()
+        {
+            path = @"Files\commands.txt";
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.UTF8))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                    CommandList.Add(new Regex($@"^{line}(\s\w)*$"));
+            }
+        }
+        public static List<Regex> GetList() => CommandList;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using PlsRespond.Classes;
+using System;
+using System.Dynamic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PlsRespond
+{
+    class Program
+    {
+        static void ReadCommand(string Message)
+        {
+            foreach(Regex substr in CommandLibrary.GetList())
+            {
+                if (substr.IsMatch(Message))
+                {
+                    Console.WriteLine($"{substr} is a command indeed");
+                    return;
+                }
+            }
+            Console.WriteLine("Command not found");
+        }
+        static void Main(string[] args)
+        {
+            string Message;
+            while (true)
+            {
+                Console.WriteLine("Type a message or a command or type :wq to exit");
+                Message = Console.ReadLine();
+                if (Message[0] == '/')
+                {
+                    ReadCommand(Message);
+                }
+                else if (Message.Equals(":wq"))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(Message + " :)");
+                }
+
+            }
+        }
+    }
+}
